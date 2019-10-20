07c8eaaf71b8548ef2e94af8a82fb310745993ce
Lunarixus <Nathanb644@hotmail.co.uk>
2019-10-20 21:04:51 +0000

    platina: Correct path to SEPolicy makefile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,7 +233,7 @@ VENDOR_SECURITY_PATCH := 2019-06-05
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
