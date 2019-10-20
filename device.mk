1b2a689d92e59985ddad30396db5236e4a6854de
Lunarixus <Nathanb644@hotmail.co.uk>
2019-10-20 22:04:56 +0000

    platina: Drop missing ANTRadio config

--- device.mk
+++ device.mk
@@ -153,9 +153,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
