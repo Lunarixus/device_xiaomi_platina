7a43e8113e9b9afa7a4d515e91d72227e5355192
Lunarixus <Nathanb644@hotmail.co.uk>
2019-10-20 23:03:50 +0000

    platina: Kang Jason SEPolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,6 +234,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
 include device/qcom/sepolicy/Android.mk
+
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
