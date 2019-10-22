a15a65f93f597bed5592401d195988af7024c21b
Lunarixus <Nathanb644@hotmail.co.uk>
2019-10-22 10:14:01 +0000

    platina: Update audio config

--- device.mk
+++ device.mk
@@ -108,10 +108,11 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
+    audiod \
     audio.a2dp.default \
+    audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
-    libaacwrapper \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
